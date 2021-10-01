4036bb0d555afff12fee804db4d44cef79ca2b59
square1grp <sq1grpdev@gmail.com>
2021-10-01 02:02:43 -0700

    complete ready

--- src/pages/Apply/Apply.jsx
+++ src/pages/Apply/Apply.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Ready from './steps/Ready';
+import styles from './Apply.module.scss';
+import { Link } from 'react-router-dom';
 
 const Apply = () => {
   return (
     <Layout>
-      Apply
+      <div className={styles.apply}>
+        <div className={styles.apply__content}>
+          <Ready />
+        </div>
+
+        <div className={styles.apply__footer}>
+          <div className={styles.apply__pp}>
+            By selecting “Continue” you agree to the<br />
+            <Link
+              to={{ pathname: '/' }}
+              dangerouslySetInnerHTML={{ __html: '<b>Rent Butter Privacy Policy.</b>' }}
+            />
+          </div>
+
+          <button className={styles.button__continue}>Continue</button>
+        </div>
+      </div>
     </Layout>
   );
 };
---
+++ src/pages/Apply/steps/Ready/Ready.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styles from './Ready.module.scss';
+import { ReactComponent as AlertIcon } from '../../../../assets/images/Alert.svg';
+import { ReactComponent as GetStartedIcon } from '../../../../assets/images/Get Started.svg';
+import imgIDHand from '../../../../assets/images/ID-Hand@2x.png';
+import { ReactComponent as FinancesIcon } from '../../../../assets/images/Finances.svg';
+import { ReactComponent as CreditCardIcon } from '../../../../assets/images/Credit Card.svg';
+
+const Ready = () => {
+  return (
+    <div className={styles.step}>
+      <div className={styles.notification}>
+        <AlertIcon className={styles.notification__icon} />
+
+        <div className={styles.notification__text}>
+          <b>Slow connection detected!</b> Please connect to WiFi before proceeding.
+        </div>
+      </div>
+
+      <div className={styles.get_started}>
+        <GetStartedIcon className={styles.get_started__icon} />
+
+        <div
+          className={styles.get_started__heading}
+          dangerouslySetInnerHTML={{ __html: '<b>Let’s get started!</b>' }}
+        />
+
+        <div
+          className={styles.get_started__sub_heading}
+          dangerouslySetInnerHTML={{ __html: 'Here’s what you’ll need:' }}
+        />
+      </div>
+
+      <div className={styles.icon__list}>
+        <div className={styles.icon__line}>
+          <img
+            className={styles.icon}
+            src={imgIDHand}
+            alt="A valid government issued ID"
+          />
+
+          <span className={styles.icon__desc} >
+            <b>A valid government issued ID</b>
+          </span>
+        </div>
+
+        <div className={styles.icon__line}>
+          <FinancesIcon className={styles.icon} />
+
+          <span className={styles.icon__desc} >
+            <b>Proof of finances/rental history</b>
+          </span>
+        </div>
+
+        <div className={styles.icon__line}>
+          <CreditCardIcon className={styles.icon} />
+
+          <span className={styles.icon__desc} >
+            <b>A credit/debit card for app fee</b>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Ready;
