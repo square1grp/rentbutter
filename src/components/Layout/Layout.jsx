e0e81b66af8e2515aa62edf62488d0fcad859a50
square1grp <sq1grpdev@gmail.com>
2021-10-01 00:58:36 -0700

    add header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '../Modal';
 import Header from '../Header';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, onBack = () => null, onExit = () => null }) => {
+  const [show, setShow] = useState(true);
+
+  const handleClickExit = () => {
+    setShow(false);
+
+    onExit();
+  };
+
   return (
-    <Modal show>
-      <Header />
+    <Modal show={show}>
+      <Header onBack={onBack} onExit={handleClickExit} />
 
       {children}
     </Modal>
